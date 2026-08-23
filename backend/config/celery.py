83420ec7218f01571968e1078d64fd8806bff040
sara-alsayyah <saraalsayyah2111978@gmail.com>
2026-08-24 01:00:59 +0300

    ..

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('backend')
+app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
